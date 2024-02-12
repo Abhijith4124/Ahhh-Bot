7ac66c416c2b22a5fad0f4db4cd208d85a2e05ea
Abhijith <abhijith4124@gmail.com>
2024-02-12 06:47:55 +0530

    Crash due to Permission Issue
    Fixed Crash when the bot does not have the permission to access or send message in a log channel

--- utils/discord/logger.js
+++ utils/discord/logger.js
@@ -1,30 +1,38 @@
 const {EmbedBuilder} = require("discord.js");
 
 async function logToGameLogChannel(client, guild, serverName, title, message) {
-    const gameLogChannelIdKey = `${guild}_${serverName.replaceAll(" ", "_")}_GameLogChannelId`;
-    const gameLogChannelId = client.db.get(gameLogChannelIdKey);
+    try {
+        const gameLogChannelIdKey = `${guild}_${serverName.replaceAll(" ", "_")}_GameLogChannelId`;
+        const gameLogChannelId = client.db.get(gameLogChannelIdKey);
 
-    if (gameLogChannelId) {
-        const channel = await client.channels.fetch(gameLogChannelId);
-        if (channel) {
-            const logEmbed = new EmbedBuilder().setColor("#ff0000").setTitle(title).setDescription(message);
-            await channel.send({embeds: [logEmbed]});
+        if (gameLogChannelId) {
+            const channel = await client.channels.fetch(gameLogChannelId);
+            if (channel) {
+                const logEmbed = new EmbedBuilder().setColor("#ff0000").setTitle(title).setDescription(message);
+                await channel.send({embeds: [logEmbed]});
+            }
+
         }
-
+    }catch (e) {
+        console.error(e);
     }
 }
 
 async function logToWhitelistLogChannel(client, guild, serverName, title, message) {
-    const whitelistLogChannelIdKey = `${guild}_${serverName.replaceAll(" ", "_")}_WhitelistLogChannelId`;
-    const whitelistLogChannelId = client.db.get(whitelistLogChannelIdKey);
+    try {
+        const whitelistLogChannelIdKey = `${guild}_${serverName.replaceAll(" ", "_")}_WhitelistLogChannelId`;
+        const whitelistLogChannelId = client.db.get(whitelistLogChannelIdKey);
 
-    if (whitelistLogChannelId) {
-        const channel = await client.channels.fetch(whitelistLogChannelId);
-        if (channel) {
-            const logEmbed = new EmbedBuilder().setColor("#ff0000").setTitle(title).setDescription(message);
-            await channel.send({embeds: [logEmbed]});
+        if (whitelistLogChannelId) {
+            const channel = await client.channels.fetch(whitelistLogChannelId);
+            if (channel) {
+                const logEmbed = new EmbedBuilder().setColor("#ff0000").setTitle(title).setDescription(message);
+                await channel.send({embeds: [logEmbed]});
+            }
+
         }
-
+    }catch (e) {
+        console.error(e);
     }
 }
 module.exports = {logToGameLogChannel, logToWhitelistLogChannel}
